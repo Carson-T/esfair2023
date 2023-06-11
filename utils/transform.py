5b2249f4ffbbf32886e2ab0016498499a32e1a3f
Carson-T <tan_biubiu@qq.com>
2023-06-11 20:04:59 +0800

    transform

--- utils/transform.py
+++ utils/transform.py
@@ -3,6 +3,7 @@ from albumentations import pytorch as AT
 
 def transform(args):
     train_transforms = albumentations.Compose([
+        albumentations.Resize(args["resize"], args["resize"]),
         albumentations.Transpose(p=0.5),
         albumentations.VerticalFlip(p=0.5),
         albumentations.HorizontalFlip(p=0.5),
@@ -12,7 +13,7 @@ def transform(args):
             albumentations.MedianBlur(blur_limit=5),
             albumentations.GaussianBlur(blur_limit=(1,5)),
             albumentations.GaussNoise(var_limit=(5.0, 30.0)),
-        ], p=0.5),
+        ], p=0.3),
 
         albumentations.OneOf([
             albumentations.OpticalDistortion(distort_limit=1.0),
@@ -20,11 +21,10 @@ def transform(args):
             albumentations.ElasticTransform(alpha=3),
         ], p=0.5),
 
-        albumentations.CLAHE(clip_limit=4.0, p=0.5),
+        albumentations.CLAHE(clip_limit=0.5, p=0.5),
         albumentations.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=20, val_shift_limit=10, p=0.5),
         albumentations.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, border_mode=0, p=0.5),
-        albumentations.Resize(args["resize"], args["resize"]),
-        albumentations.CoarseDropout(max_holes=1, max_height=int(args["resize"] * 0.2), max_width=int(args["resize"] * 0.2), min_height=1, min_width=1, p=0.5),
+        albumentations.CoarseDropout(max_holes=1, max_height=int(args["resize"] * 0.1), max_width=int(args["resize"] * 0.1), min_height=1, min_width=1, p=0.3),
         albumentations.Normalize(),
         AT.ToTensorV2()
         ])
